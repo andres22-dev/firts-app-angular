366780add0a4454acfff46c7bbcbb3a3d547f8b2
andres22 <afpinchao8@misena.edu.co>
2021-01-29 18:40:34 -0500

    agregando datos de un objeto a un componente y agregando un componente dentro de otro

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {Item} from '../../models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items:Item[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+    this.items = [{
+
+        id: 0,
+        title: 'manzana',
+        price: 10.5,
+        quantity: 4,
+        completed:false
+    },{
+
+      id:1,
+      title: 'mango',
+      price: 11.4,
+      quantity: 3,
+      completed:true
+    
+    }];
+
+
+  }
+
+}
